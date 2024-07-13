6464d5443239665522f79b8d9eef4b09dfb0a6fe
Nazwa Aura Rahma <115673074+NazwaAuraRahma@users.noreply.github.com>
2024-07-13 20:40:33 +0700

    Update BeritaController.php

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class BeritaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('berita.index');
+    }
+
+    public function berita1()
+    {
+        return view('berita.berita1');
+    }
+
+    public function berita2()
+    {
+        return view('berita.berita2');
+    }
+
+    public function berita3()
+    {
+        return view('berita.berita3');
+    }
+
+    public function berita4()
+    {
+        return view('berita.berita4');
+    }
+
+    public function berita5()
+    {
+        return view('berita.berita5');
+    }
+
+    public function berita6()
+    {
+        return view('berita.berita6');
+    }
 }
